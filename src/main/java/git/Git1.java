4c3224dc031a72abe4da10c9be31f3922fc5060c
greg <greg@example.com>
2020-02-15 16:40:56 +0100

    dodannie metody minus

--- src/main/java/git/Git1.java
+++ src/main/java/git/Git1.java
@@ -6,7 +6,10 @@ public class Git1 {
     private static int add(int a, int b) {
         return a + b;
     }
-    
+
+    private static int minus(int a, int b) {
+        return a - b;
+    }
     public static void main(String[] args) {
 
 
